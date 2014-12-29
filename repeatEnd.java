81202554af03336b6e4227cf7fcfeb00b834f4e3
Joseph Zhong <joseph.zhong@hotmail.com>
2014-12-28 19:13:11 -0800

    Create repeatEnd.java

---
+++ repeatEnd.java
@@ -0,0 +1,9 @@
+public String repeatEnd(String str, int n) {
+  String finalString = "";
+  for(int i = 0; i < n; i++)
+  {
+      finalString += str.substring(str.length() - n, str.length());
+  }
+  
+  return finalString;
+}
